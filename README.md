0afb1ef07b7d24287625f02bd0c7db453d7f380f
tomislavkuvek <tomislav.kuvek@thespian.hr>
2023-03-24 10:12:37 +0100

    Add readme, add deploy commands and settings

---
+++ README.md
@@ -0,0 +1,4 @@
+## DEPLOY INSTRUCTIONS
+1. Install dependencies ```npm install || yarn install```
+2. On ```main``` branch run command ```npm run deploy ||  yarn deploy```
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import checker from 'vite-plugin-checker'
-import eslint from 'vite-plugin-eslint'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import checker from 'vite-plugin-checker';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    checker({
-      typescript: true
-    }),
-    eslint()
-  ]
-})
+	base: '/',
+	plugins: [
+		react(),
+		checker({
+			typescript: true
+		}),
+		eslint()
+	]
+});
