abfa4e71736f0c1c0ae9d9f8c68938eced687b94
Zahnentferner <zahnentferner@gmail.com>
2023-04-02 07:44:54 -0300

    Add TokenJay

--- src/components/elements/cards/DappCard.tsx
+++ src/components/elements/cards/DappCard.tsx
@@ -1,9 +1,6 @@
-// import ArrowButton from '../../blocks/svg/ArrowButton';
-
 interface Props {
   cardTitle: string
   cardText: string
-//   cardHref: string
   children: React.ReactNode;
 }
 
@@ -17,12 +14,6 @@ const DappCard = (props: Props): JSX.Element => {
 				</p>
 			</div>
 			{props.children};
-			{/* <div className="px-6 pt-4 pb-2 m-5">
-				<div className='flex flex-row align-middle my-6'>
-					<a href={props.cardHref} target="_blank" rel="noreferrer"><p className='text-white mt-0 lg:mt-5 m-0 lg:m-3 md:mt-4 md:mx-4'>Launch {props.cardTitle}</p></a>
-					<span className='hidden lg:block md:block'><ArrowButton onClick={() => window.open(props.cardHref, '_blank')}/></span>
-				</div>
-			</div> */}
 		</div>
 	);
 };
--- src/components/pages/dapps/Dapps.tsx
+++ src/components/pages/dapps/Dapps.tsx
@@ -2,8 +2,8 @@ import DappCard from '../../elements/cards/DappCard';
 import DappLink from '../../elements/cards/DappLink';
 
 export default function Dapps (): JSX.Element {
-	const cardTextMilkomeda = 'Interact with Djed on the Milkomeda-C1\nEVM-Compatible Sidechain of Cardano.\n\nTestnet! (Mainnet coming soon.)';
-	const cardTextErgo = 'Interact with SigmaUSD,\nthe first ever deployment of Djed,\non the Ergo Blockchain.';
+	const cardTextMilkomeda = 'Interact with Djed on the Milkomeda-C1\nEVM-Compatible Sidechain of Cardano.\nTestnet! (Mainnet coming soon.)';
+	const cardTextErgo = 'Interact with SigmaUSD,\nthe first ever deployment of Djed,\non the Ergo Blockchain.\nChoose one of the following interfaces:';
 
 	return (
 		<div className="py-20 my-20 items-center" id="djed_apps">
@@ -11,15 +11,13 @@ export default function Dapps (): JSX.Element {
 			<h2 className='dappsTitle mb-20'>Djed</h2>
 			<div className="flex align-middle justify-center">
 				<DappCard cardTitle="SigmaUSD" cardText={cardTextErgo}>
-					<DappLink text="SigmaUSD" href="https://sigmausd.io/#/" />
+					<DappLink text="SigmaUSD.io" href="https://sigmausd.io/#/" />
+					<DappLink text="TokenJay.app" href="https://tokenjay.app/app/#ageusd" />
 				</DappCard>
 				<DappCard cardTitle="Milkomeda-C1 Djed Dollar" cardText={cardTextMilkomeda}>
-					<DappLink text="Milkomed-C1 Djed Dollar" href="https://milkomeda-c1-testnet.djed.one/" />
+					<DappLink text="milkomeda-c1-testnet.djed.one" href="https://milkomeda-c1-testnet.djed.one/" />
 				</DappCard>
 			</div>
-			{/* <div className='item-center align-middle justify-center'>
-				<p className='contactParagraph my-10'>&#x2022; Contact us to have DJED on your blockchain</p>
-			</div> */}
 		</div>
 	);
 }
