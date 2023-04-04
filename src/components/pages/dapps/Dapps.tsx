4043089e5ecc9edba55a842f6234fcac702dd560
Zahnentferner <zahnentferner@gmail.com>
2023-04-03 21:16:32 -0300

    Add COTI's Djed

--- src/components/pages/dapps/Dapps.tsx
+++ src/components/pages/dapps/Dapps.tsx
@@ -2,8 +2,9 @@ import DappCard from '../../elements/cards/DappCard';
 import DappLink from '../../elements/cards/DappLink';
 
 export default function Dapps (): JSX.Element {
+	const cardTextErgo = 'Interact with SigmaUSD,\nthe first deployment of Djed,\non the Ergo Blockchain.';
+	const cardTextCOTI = 'Interact with Djed implemented in Plutus on the Cardano Blockchain.';
 	const cardTextMilkomeda = 'Interact with Djed on the Milkomeda-C1\nEVM-Compatible Sidechain of Cardano.\nTestnet! (Mainnet coming soon!)';
-	const cardTextErgo = 'Interact with SigmaUSD,\nthe first ever deployment of Djed,\non the Ergo Blockchain.';
 
 	return (
 		<div className="py-20 my-20 items-center" id="djed_apps">
@@ -13,6 +14,9 @@ export default function Dapps (): JSX.Element {
 				<DappCard cardTitle="SigmaUSD" cardText={cardTextErgo}>
 					<DappLink text="sigmausd.io" href="https://sigmausd.io/#/" />
 					<DappLink text="tokenjay.app" href="https://tokenjay.app/app/#ageusd" />
+				</DappCard>
+				<DappCard cardTitle="COTI&apos;s Djed" cardText={cardTextCOTI}>
+					<DappLink text="djed.xyz" href="https://djed.xyz/" />
 				</DappCard>
 				<DappCard cardTitle="Milkomeda-C1 Djed Dollar" cardText={cardTextMilkomeda}>
 					<DappLink text="milkomeda-c1.djed.one" href="https://milkomeda-c1-testnet.djed.one/" />
