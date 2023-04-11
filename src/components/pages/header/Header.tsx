424b3082c72b33a809bfa3cb4f539314e5a0f771
tomislavkuvek <tomislav.kuvek@thespian.hr>
2023-04-11 13:37:12 +0200

    Add animation and menu on mobile

--- src/components/pages/header/Header.tsx
+++ src/components/pages/header/Header.tsx
@@ -2,6 +2,7 @@ import Logo from '../../../assets/djed-logo.png';
 import Discord from '../../../assets/discordLogo.png';
 import GitHub from '../../../assets/githubLogo.png';
 import Twitter from '../../../assets/Twitter.png';
+import { useState } from 'react';
 
 const Header: React.FC = () => {
 
@@ -19,35 +20,34 @@ const Header: React.FC = () => {
 
 	};
 
-	// const btn = document.getElementById('navbar-button');
-	// const menu = document.getElementById('navbar-default');
-	
-	// if (menu && btn) {
-	// 	btn.addEventListener('click', () => {
-	// 		menu.classList.toggle('hidden');
-	// 	});
-	// }
+	const [isOpen, setIsOpen] = useState(false);
+
+	function toggleMenu() {
+		setIsOpen(!isOpen);
+	}
 
 	return (
-		<nav className="sticky top-0 w-full px-2 py-1 hover:bg-stone-900/90 lg:bg-transparent bg-stone-900/90 sm:px-4 z-50">
-			<div className="flex items-center justify-between mx-auto align-middle">
+		<nav className="sticky top-0 hover:bg-stone-900/90 lg:bg-transparent bg-stone-900/90 border-gray-200 z-50">
+			<div className="flex flex-wrap items-center justify-between mx-auto p-4">
 				<a href="#">
 					<span className="text-2xl font-extrabold navColor"><img src={Logo} className="w-12 ml-6" /></span>
 				</a>
-				<button id="navbar-button" data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+				<button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded={isOpen} onClick={toggleMenu}>
 					<span className="sr-only">Open main menu</span>
 					<svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
 				</button>
-				<div className="hidden w-full md:block md:w-auto flex-row" id="navbar-default">
-					<ul className="hidden space-x-2 md:inline-flex mr-6 align-middle justify-center items-center">
+				<div className={`${!isOpen ? 'hidden' : 'block'} w-full md:block md:w-auto`} id="navbar-default">
+					<ul className={`${!isOpen ? 'hidden' : 'flex-col'} space-y-5 lg:space-y-0 md:inline-flex mr-6 align-middle justify-center items-center lg:inline-flex`}>
 						<li><a id="homeLink" className="active px-4 py-2 font-semibold navColor cursor-pointer" onClick={() => handleActive('homeLink', '#')}>HOME</a></li>
 						<li><a id="appsLink" className="px-4 py-2 font-semibold navColor cursor-pointer" onClick={() => handleActive('appsLink', 'djed_apps')}>DJED</a></li>
 						<li><a id="contributorsLink" className="px-4 py-2 font-semibold navColor cursor-pointer" onClick={() => handleActive('contributorsLink', 'contributors')}>CONTRIBUTORS</a></li>
 						<li><a href="https://docs.djed.one/alliance/the-djed-alliance" target="_blank" className="px-4 py-2 font-semibold navColor" rel="noreferrer">DOCS</a></li>
 						<li><a href="https://djed.medium.com/" target="_blank" className="px-4 py-2 font-semibold navColor" rel="noreferrer">BLOG</a></li>
-						<li><a href="https://discord.com/invite/5TWZwGXXym" target="_blank" rel="noreferrer"><img className="socialImage mx-2 cursor-pointer" src={Discord} /></a></li>
-						<li><a href="https://github.com/DjedAlliance" target="_blank" rel="noreferrer"><img className="socialImage mx-2 cursor-pointer" src={GitHub} /></a></li>
-						<li><a href="https://twitter.com/DjedAlliance" target="_blank" rel="noreferrer"><img className="socialImage mx-2 cursor-pointer" src={Twitter} /></a></li>
+						<ul className="flex justify-center">
+							<li><a href="https://discord.com/invite/5TWZwGXXym" target="_blank" rel="noreferrer"><img className="socialImage mx-2 cursor-pointer" src={Discord} /></a></li>
+							<li><a href="https://github.com/DjedAlliance" target="_blank" rel="noreferrer"><img className="socialImage mx-2 cursor-pointer" src={GitHub} /></a></li>
+							<li><a href="https://twitter.com/DjedAlliance" target="_blank" rel="noreferrer"><img className="socialImage mx-2 cursor-pointer" src={Twitter} /></a></li>
+						</ul>
 					</ul>
 				</div>
 			</div>
