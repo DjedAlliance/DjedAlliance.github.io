8a49f93b050ed132e57fdc2bf1c029d51d13e6e7
Zahnentferner <zahnentferner@gmail.com>
2023-04-28 21:14:10 +0000

    Link to Milkomeda-C1 mainnet deployment

--- src/components/pages/dapps/Dapps.tsx
+++ src/components/pages/dapps/Dapps.tsx
@@ -4,7 +4,7 @@ import DappLink from '../../elements/cards/DappLink';
 export default function Dapps (): JSX.Element {
 	const cardTextErgo = 'Interact with SigmaUSD,\nthe first deployment of Djed,\non the Ergo Blockchain.';
 	const cardTextCOTI = 'Interact with COTI\'s Djed\nimplemented in Plutus\non the Cardano Blockchain.';
-	const cardTextMilkomeda = 'Interact with Djed on the Milkomeda-C1\nEVM-Compatible Sidechain of Cardano.\nTestnet! (Mainnet coming soon!)';
+	const cardTextMilkomeda = 'Interact with Djed on\nthe Milkomeda-C1\nEVM Sidechain of Cardano.';
 
 	return (
 		<div className="py-20 my-20 items-center" id="djed_apps">
@@ -19,7 +19,7 @@ export default function Dapps (): JSX.Element {
 					<DappLink text="djed.xyz" href="https://djed.xyz/" />
 				</DappCard>
 				<DappCard cardTitle="Milkomeda-C1 Djed Dollar" cardText={cardTextMilkomeda}>
-					<DappLink text="milkomeda-c1.djed.one" href="https://milkomeda-c1-testnet.djed.one/" />
+					<DappLink text="milkomeda-c1.djed.one" href="https://milkomeda-c1.djed.one/" />
 				</DappCard>
 			</div>
 		</div>
