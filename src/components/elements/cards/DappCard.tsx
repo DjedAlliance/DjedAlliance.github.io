beef422e89c4b230d10ebf9693eba0b02c1b0716
Zahnentferner <zahnentferner@gmail.com>
2024-07-29 19:49:51 +0300

    Put back noreferrer

--- src/components/elements/cards/DappCard.tsx
+++ src/components/elements/cards/DappCard.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const DappCard = (props: Props): JSX.Element => {
 	return (
-		<a href={props.href} target="_blank">
+		<a href={props.href} target="_blank" rel="noreferrer">
 			<div className="card rounded-lg overflow-hidden shadow-lg border z-30 lg:m-10 m-5 lg:w-1/3 sm:w-96">
 				<div className="px-6 py-4">
 					<h3 className="cardTitle font-bold whitespace-normal text-md lg:text-xl mb-2 py-2 lg:whitespace-pre">{props.cardTitle}</h3>
